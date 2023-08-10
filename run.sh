857120532acda2f8a88bd4500a6e39c0f8767bb4
rbudai <robert.budai@analog.com>
2023-08-10 13:17:59 +0200

    run: rename app
    Signed-off-by: rbudai <robert.budai@analog.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # In case previous container is running needs removeing before starting the new one
-sudo docker container rm -f /my-running-app
+sudo docker container rm -f /tof-web-app
 # Building web server for amr64 in container
 sudo docker build -t my-apache2 .
 # Starting webserver
-sudo docker run -dit --name my-running-app -p 8080:80 my-apache2
+sudo docker run -dit --name tof-web-app -p 8080:80 my-apache2
