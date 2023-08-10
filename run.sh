a8d6a38973d17c150344a79f53b651992b700497
rbudai <robert.budai@analog.com>
2023-08-10 13:02:00 +0200

    run.sh: remove container before starting the following one
    Signed-off-by: rbudai <robert.budai@analog.com>

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+# In case previous container is running needs removeing before starting the new one
+sudo docker container rm -f /my-running-app
+# Building web server for amr64 in container
 sudo docker build -t my-apache2 .
+# Starting webserver
 sudo docker run -dit --name my-running-app -p 8080:80 my-apache2
