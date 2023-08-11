caccaa5aa9d182e3678aaca335186c54ac3fae6b
rbudai <robert.budai@analog.com>
2023-08-11 11:16:06 +0000

    indx,js: adde socket connect/deconnect messages
    Signed-off-by: rbudai <robert.budai@analog.com>

--- tofWebApp/index.js
+++ tofWebApp/index.js
@@ -2,25 +2,30 @@
         const refreshDeviceBtn = document.getElementById("refreshDevice");
         const setFrameTypeBtn = document.getElementById("setFrameType");
 
+        // Create WebSocket connection
+        var socket = new WebSocket("ws://10.42.0.1:5000", "network-protocol");
+        // Listen for possible errors
+        socket.addEventListener("error", (event) => {
+                console.log("WebSocket error: ", event);
+        });
+
+        socket.onopen = async function(event) {
+                console.log("Connected to server");
+        }
+        socket.onclose = async function(event){
+                console.log("Disconnected from server");
+        }
+
+        socket.onmessage = async function (event) {
+
+                console.log("Receiving message from server...");
+        }
 
         window.addEventListener("load", (event) => {
-                // Create WebSocket connection
-                socket = new WebSocket("ws://10.42.0.1:5000", "network-protocol");
-                // Listen for possible errors
-                socket.addEventListener("error", (event) => {
-                        console.log("WebSocket error: ", event);
-                });
-
                 protobuf.load("resources/buffer.proto");
         });
 
         refreshDeviceBtn.addEventListener("click", () => {
-                // Create WebSocket connection
-                socket = new WebSocket("ws://10.42.0.1:5000", "network-protocol");
-                // Listen for possible errors
-                socket.addEventListener("error", (event) => {
-                        console.log("WebSocket error: ", event);
-                });
         });
 
 
